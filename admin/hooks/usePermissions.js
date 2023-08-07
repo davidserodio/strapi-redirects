fa333cfde829f80e811cc6785e1f957bbadd14d7
David Serôdio <davidserodio@hotmail.com>
2023-08-07 17:45:49 +0100

    feat: redirect if no permissions

---
+++ admin/hooks/usePermissions.js
@@ -0,0 +1,11 @@
+import { useRBAC } from '@strapi/helper-plugin';
+import pluginId from './helpers/pluginId';
+
+const perms = { read: [{ action: `plugin::${pluginId}.read`, subject: null }] };
+
+function usePermissions() {
+  const { allowedActions, isLoading: loading } = useRBAC(perms);
+  return { ...allowedActions, loading };
+}
+
+export default usePermissions;
--- admin/screens/Redirect/index.js
+++ admin/screens/Redirect/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { NotFound } from '@strapi/helper-plugin';
 import pluginId from '../../helpers/pluginId';
+import usePermissions from '../../hooks/usePermissions';
 
 import OverviewPage from './OverviewPage';
 import DetailPage from './DetailPage';
 
 const App = () => {
+  const { canRead, loading } = usePermissions();
+
+  if (loading) return null;
+
+  if (!canRead) return <Redirect to="/" />;
+
   return (
     <div>
       <Switch>
