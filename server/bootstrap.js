40a56cb11b8b720d906d56ab5859891783c353e1
David Serôdio <davidserodio@hotmail.com>
2023-08-07 15:54:02 +0100

    fix: module.exports

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,15 +1,16 @@
-import { Strapi } from "@strapi/strapi";
 import pluginId from './util/pluginId';
 
-export default async ({ strapi }) => {
-  const actions = [
-    {
-      section: 'plugins',
-      displayName: 'Access redirects menu',
-      uid: 'read',
-      pluginName: pluginId,
-    }
-  ];
+module.exports = {
+  default: async ({ strapi }) => {
+    const actions = [
+      {
+        section: 'plugins',
+        displayName: 'Access redirects menu',
+        uid: 'read',
+        pluginName: pluginId,
+      }
+    ];
 
-  await strapi.admin.services.permission.actionProvider.registerMany(actions);
-};
+    await strapi.admin.services.permission.actionProvider.registerMany(actions);
+  }
+}
