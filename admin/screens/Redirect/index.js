5a41fc628ca3bd750a524649aaa487819dce15b5
David Serôdio <davidserodio@hotmail.com>
2023-08-07 17:57:02 +0100

    fix: Redirect import

--- admin/screens/Redirect/index.js
+++ admin/screens/Redirect/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { NotFound } from '@strapi/helper-plugin';
 import pluginId from '../../helpers/pluginId';
 import usePermissions from '../../hooks/usePermissions';
