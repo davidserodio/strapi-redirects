0e357eb40a331fc97b3a2aded1911acfa100771b
David Serôdio <davidserodio@hotmail.com>
2023-08-07 16:05:20 +0100

    fix: function

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,16 +1,15 @@
 const pluginId = require('./util/pluginId');
 
-module.exports = {
-  default: async ({ strapi }) => {
+module.exports = async ({ strapi }) => {
     const actions = [
-      {
+        {
         section: 'plugins',
         displayName: 'Access redirects menu',
         uid: 'read',
         pluginName: pluginId,
-      }
+        }
     ];
 
     await strapi.admin.services.permission.actionProvider.registerMany(actions);
-  }
 }
+
